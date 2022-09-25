c7ef9982bfe74514a6661daada44e8411c94464b
nacfson <nacfson@gmail.com>
2022-09-25 13:36:33 +0900

    hard

--- Assets/01_Scripts/Yu/Player/PlayerRightClickInteraction.cs
+++ Assets/01_Scripts/Yu/Player/PlayerRightClickInteraction.cs
@@ -9,7 +9,6 @@ public class PlayerRightClickInteraction : AgentInteraction
 
     [SerializeField] private GameObject _block;
 
-    public float blockSize = 10f;
 
 
     [SerializeField]
@@ -27,51 +26,42 @@ public class PlayerRightClickInteraction : AgentInteraction
     }
     public override void Interact(GameObject obj)
     {
+        if(CanInteract)
+        {
+            if(Input.GetMouseButtonDown(1))
+            {
+                SetBlock();
+            }
 
+            
+        }
     }
     protected override void CheckCanInteract()
     {
         
     }
 
-    void SetBlock(Vector3 vector)
+    void SetBlock()
     {
-        //Vector3 temp = hit.transform.position;
-        //Vector3 pos = new Vector3(temp.x,temp.y + UnityEditor.EditorSnapSettings.move.y,temp.z);
-        Instantiate(_block,vector,Quaternion.identity);
+        Vector3 temp = hit.transform.position;
+        Vector3 pos = new Vector3(temp.x,temp.y + UnityEditor.EditorSnapSettings.move.y,temp.z);
+        Instantiate(_block,pos,Quaternion.identity);
         Debug.Log("SetBlock");
     }
 
-    public void CheckGameObject(Vector3 vector)
+    public void CheckGameObject(GameObject obj)
     {
-            if(Input.GetMouseButtonDown(1))
-            {
-                SetBlock(vector);
-            }
+        Interact(obj);
     }
     public override void CheckRay()
     {
         int layerMask = (-1) - (1 << LayerMask.NameToLayer("Player"));
-        // Vector3 pos = new Vector3(_controller.transform.position.x  ,_controller.transform.position.y + 10f , _controller.transform.position.z); 
-        // CanInteract = Physics.Raycast(pos,_controller.Camera.transform.forward ,out hit, 40f,layerMask);
-        // Debug.DrawRay(pos, _controller.Camera.transform.forward * 40f, Color.green);
-
-        Ray ray = new Ray(pos, _controller.Camera.transform.forward);
-        RaycastHit raycastHit;
-        if (Physics.Raycast(ray, out raycastHit))
+        Vector3 pos = new Vector3(_controller.transform.position.x  ,_controller.transform.position.y + 10f , _controller.transform.position.z); 
+        CanInteract = Physics.Raycast(pos,_controller.Camera.transform.forward ,out hit, 40f,layerMask);
+        Debug.DrawRay(pos, _controller.Camera.transform.forward * 40f, Color.green);
+        if(CanInteract)
         {
-            Vector3 directionVector = raycastHit.transform.position - ray.GetPoint(raycastHit.distance);
-            directionVector *= -2; //���⺤�Ϳ��� Ư�� �� ���� �ٶ󺸰� �ִ� ���� ���� 0.5�� �����Ƿ� �������� int������ ���ֹ����� ����
-            Vector3 blockDirectionVector = new Vector3((int)directionVector.x, (int)directionVector.y, (int)directionVector.z);
-            blockDirectionVector *= blockSize;
-            CheckGameObject(blockDirectionVector);
-            Debug.Log(blockDirectionVector);
-
-        
+            CheckGameObject(hit.collider.gameObject);
         }
-        // if(CanInteract)
-        // {
-        //     CheckGameObject(hit.collider.gameObject);
-        // }
     }
 }
