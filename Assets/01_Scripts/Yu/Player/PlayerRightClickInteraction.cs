cf822298c1f19df9611d5d8231cebe17d938e178
nacfson <nacfson@gmail.com>
2022-09-25 11:44:34 +0900

    SetBlock

--- Assets/01_Scripts/Yu/Player/PlayerRightClickInteraction.cs
+++ Assets/01_Scripts/Yu/Player/PlayerRightClickInteraction.cs
@@ -8,10 +8,13 @@ public class PlayerRightClickInteraction : AgentInteraction
     private PlayerController _controller;
     public UnityEvent<GameObject> LeftClickEvent;
 
-    [SerialzieField] private GameObjct _block;
+    [SerializeField] private GameObject _block;
 
     [SerializeField]
     private string _defineName;
+
+    public Camera cam;
+    RaycastHit hit;
     private void Awake()
     {
         _controller = GetComponent<PlayerController>();
@@ -26,7 +29,7 @@ public class PlayerRightClickInteraction : AgentInteraction
         {
             if(Input.GetMouseButtonDown(1))
             {
-
+                SetBlock();
             }
 
             
@@ -37,23 +40,30 @@ public class PlayerRightClickInteraction : AgentInteraction
         
     }
 
+    void SetBlock()
+    {
+        Vector3 temp = hit.collider.transform.position;
+        
+        Vector3 pos = new Vector3(temp.x,temp.y + 10f,temp.z);
+        Instantiate(_block,pos,Quaternion.identity);
+        Debug.Log("SetBlock");
+
+
+    }
+
     public void CheckGameObject(GameObject obj)
     {
         Interact(obj);
     }
     public override void CheckRay()
     {
-        RaycastHit hit;
+        
         Vector3 pos = new Vector3(_controller.transform.position.x,_controller.transform.position.y + 10f, _controller.transform.position.z); 
         CanInteract = Physics.Raycast(pos,_controller.Camera.transform.forward ,out hit, 40f);
         if(CanInteract)
         {
             CheckGameObject(hit.collider.gameObject);
-
         }
-
-
-        
         Debug.DrawRay(pos, _controller.Camera.transform.forward* 40f, Color.green);
     }
 }
