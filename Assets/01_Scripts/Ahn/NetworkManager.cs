baf02f46b6df3406188f9c80bfb2ef166c1e975b
dksgudwn <dksgudwn620@gmail.com>
2022-11-02 11:33:03 +0900

    ohnojo

--- Assets/01_Scripts/Ahn/NetworkManager.cs
+++ Assets/01_Scripts/Ahn/NetworkManager.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
 
-public class NetworkManager : MonoBehaviour
+public class NetworkManager : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
+    public InputField NickNameInput;
+    
+    
+    
+    void Awake()
     {
-        
+        Screen.SetResolution(960, 540, false);
+        PhotonNetwork.SendRate = 60;
+        PhotonNetwork.SerializationRate = 30;
     }
+    public void Connect() => PhotonNetwork.ConnectUsingSettings();
 
-    // Update is called once per frame
-    void Update()
+    public override void OnConnectedToMaster()
     {
-        
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions {MaxPlayers = 6}, null);
+    }
+    public override void OnJoinedRoom()
+    {
+        LoadingSceneController.LoadScene("Ohnozo'sServerTestRoom");
+        spawn();
+    }
+    public void spawn()
+    {
+        //PhotonNetwork.Instantiate("Player", new Vector3(Random.Range(-6, 10), Random.Range(0, 10), 0), Quaternion.identity);
     }
 }
